0b53ada9d12b9b0f84880c87e026be68426bc9ed
Arvinth C <54614142+ArvinthC3000@users.noreply.github.com>
2021-06-27 15:02:34 +0530

    Delete task

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,15 +1,24 @@
 import PropTypes from 'prop-types';
+import { FaTimes } from 'react-icons/fa';
 
-const Task = ({ task }) => {
+const Task = ({ task, onDelete }) => {
   return (
     <div className='task'>
-      <h3>My Task</h3>
+      <h3>
+        {task.text}{' '}
+        <FaTimes
+          style={{ color: 'red', cursor: 'pointer' }}
+          onClick={onDelete}
+        />
+      </h3>
+      <p>{task.day}</p>
     </div>
   );
 };
 
 Task.propTypes = {
   task: PropTypes.object.isRequired,
+  onDelete: PropTypes.func.isRequired,
 };
 
 export default Task;
--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,18 +1,19 @@
 import PropTypes from 'prop-types';
 import Task from './Task';
 
-const Tasks = ({ tasks }) => {
+const Tasks = ({ tasks, onDelete }) => {
   return (
     <>
       {tasks.map(task => (
-        <Task key={task.id} task={task} />
+        <Task key={task.id} task={task} onDelete={onDelete} />
       ))}
     </>
   );
 };
 
-// Tasks.propTypes = {
-//   tasks: PropTypes.object.isRequired,
-// };
+Tasks.propTypes = {
+  tasks: PropTypes.object.isRequired,
+  onDelete: PropTypes.func.isRequired,
+};
 
 export default Tasks;
